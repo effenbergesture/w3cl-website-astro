b9f2d259dbd28760cc469f129ecac568422dcba1
effenbergesture <effenbergesture@proton.me>
2024-10-30 07:57:41 -0700

    refactor: update AppConfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Chip Leader Stake',
+  title: 'Chip Leader Stake',
+  description: '',
+  author: 'effenbergesture',
   locale_region: 'en-us',
   locale: 'en',
 };
